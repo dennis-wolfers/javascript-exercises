49b06171ea97c35df4043c887477d2e6fdf5400c
Dennis Wolfers <dennis.wolfers@gmail.com>
2024-02-26 14:27:25 -0800

    implemented 1-7, all tests passing

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...values) {
+    let newArray = [];
 
+    for (let value of values) {
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] !== value) {
+                newArray.push(array[i]);
+            }
+        }
+        array = newArray;
+        newArray = [];
+    }
+
+    return array;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    if (typeof num1 !== "number" || 
+        typeof num2 !== "number" || 
+        num1 < 0 || 
+        num2 < 0) return "ERROR";
 
+    const smallerNumber = num1 < num2 ? num1 : num2;
+    const largerNumber = num1 > num2 ? num1 : num2;
+
+    for (let i = smallerNumber; i <= largerNumber; i++) {
+        sum += i;
+    }
+    
+    return sum;
 };
 
 // Do not edit below this line
