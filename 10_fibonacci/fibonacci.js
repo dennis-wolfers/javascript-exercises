1ef2164b7655ecf56e62c17b85a2c5398a5a0560
Dennis Wolfers <dennis.wolfers@gmail.com>
2024-04-29 15:05:32 -0700

    Implement exercise 10, fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = parseInt(num);
+    // return "OOPS" for negative numbers
+    if (num < 0) {
+        return "OOPS";
+    }
+    if (num === 0) {
+        return 0;
+    }
 
+    let prev = 0, curr = 1, next;
+    for (let i = 2; i <= num; i++) {
+        next = prev + curr;
+        prev = curr;
+        curr = next;
+    }
+    
+    return curr;
 };
 
 // Do not edit below this line
