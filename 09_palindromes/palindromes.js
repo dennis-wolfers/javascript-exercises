117f6afbd145e3ad9fa49d9ead2906fdb0e6fa68
Dennis Wolfers <dennis.wolfers@gmail.com>
2024-04-29 14:22:52 -0700

    Implement exercise 9, palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (pal) {
+    // remove white space and non-alphanumeric characters
+    pal = pal.replace(/[\W_]/g, '').toLowerCase();
+    // make a reversed version of the string
+    reversed = pal.split('').reverse().join('');
+    return pal === reversed;
 };
 
 // Do not edit below this line
