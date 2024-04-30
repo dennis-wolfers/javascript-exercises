9dce14c9bbb6a523eaebf6f7a255b6ee05447e09
Dennis Wolfers <dennis.wolfers@gmail.com>
2024-04-30 13:39:41 -0700

    Implement exercise 11, findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldest, person) => {
+        if (oldest === undefined) {
+            return person;
+        }
+        const personLatestYear = person?.yearOfDeath ?? 2024;
+        const personAge = personLatestYear - person.yearOfBirth;
+        const oldestLatestYear = oldest?.yearOfDeath ?? 2024;
+        const oldestAge = oldestLatestYear - oldest.yearOfBirth;
+        if (personAge > oldestAge) {
+            return person;
+        }
+        return oldest;
+    }, undefined);
 };
 
 // Do not edit below this line
